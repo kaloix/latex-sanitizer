93e884f50b8933a65d64191595ad35a2963175f1
Stefan Schindler <stefan@lifebook>
2016-05-14 10:39:11 +0200

    process text by line

--- sanitize.py
+++ sanitize.py
@@ -1,47 +1,52 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
 
-import string
+import fileinput
 import sys
 
-# read stdin
-text = sys.stdin.read()
+REPLACE = {'“': '``',
+           '”': '\'\'',
+           '’': '\'',
+           '\\': '\\textbackslash ',
+           '*': '\\textasteriskcentered ',
+           '_': '\\_',
+           '#': '\\#',
+           '$': '\\$',
+           '%': '\\%',
+           '{': '\\{',
+           '}': '\\}',
+           '&': '\\&',
+           '…': '\\dots ',
+           '~': '\\~{}',
+           '^': '\\^{}'}
+DELETE = ''
 
-# replace printable characters
-replacement = [
-	('“', '``'),
-	('”', '\'\''),
-	('’', '\''),
-	('\\', '\\textbackslash '),
-	('*', '\\textasteriskcentered '),
-	('_', '\\_'),
-	('#', '\\#'),
-	('$', '\\$'),
-	('%', '\\%'),
-	('{', '\\{'),
-	('}', '\\}'),
-	('&', '\\&'),
-	('…', '\\dots '),
-	('~', '\\~{}'),
-	('^', '\\^{}')
-]
-illegal = set()
-for old, new in replacement:
-	if old in text:
-		text = text.replace(old, new)
-		illegal.add(old)
-if illegal:
-	print('Replaced characters: "{}"'.format(''.join(illegal)), file=sys.stderr)
 
-# delete special characters
-allowed = set(string.ascii_letters)
-allowed |= set(string.digits)
-allowed |= set(string.punctuation)
-allowed.add('\n')
-illegal = set(text) - allowed
-for char in illegal:
-	text = text.replace(char, ' ')
-if illegal:
-	print('Deleted characters: "{}"'.format(''.join(illegal)), file=sys.stderr)
+def main():
+    all_replaced, all_deleted = set(), set()
+    for line in fileinput.input():
+        line, replaced = replace(line, REPLACE)
+        all_replaced.update(replaced)
+        line, deleted = delete(line, DELETE)
+        all_deleted.update(deleted)
+        sys.stdout.write(line)
+    print('Replaced characters: {}'.format(' '.join(sorted(all_replaced))),
+          file=sys.stderr)
+    print('Deleted characters: {}'.format(' '.join(sorted(all_deleted))),
+          file=sys.stderr)
 
-# write stdout
-print(text)
+
+def replace(text, table):
+    replaced = {char for char in table if char in text}
+    text = text.translate(str.maketrans(table))
+    return text, replaced
+
+
+def delete(text, illegals):
+    deleted = {char for char in illegals if char in text}
+    table = {char: None for char in illegals}
+    text = text.translate(str.maketrans(table))
+    return text, deleted
+
+
+if __name__ == '__main__':
+    main()
